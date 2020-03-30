48a711206b5aa890f1b3c3df0fd9821191f87f00
James Tony <952121091@qq.com>
2020-03-30 22:07:15 +0800

    提交ffmpeg 文件

--- app/src/main/java/com/jamestony/ffmpeg_diary/MainActivity.kt
+++ app/src/main/java/com/jamestony/ffmpeg_diary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jamestony.ffmpeg_diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
     }
